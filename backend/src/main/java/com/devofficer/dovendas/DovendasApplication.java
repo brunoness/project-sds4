31618d6cf6fa63b15a3f1497bf128d54b830ca3d
Bruno Correia <bruno.h.c@hotmail.com>
2021-09-07 14:32:46 -0300

    New Project

---
+++ backend/src/main/java/com/devofficer/dovendas/DovendasApplication.java
@@ -0,0 +1,13 @@
+package com.devofficer.dovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DovendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DovendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devofficer/dovendas/DovendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devofficer.dovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DovendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
