68cc5850181dbced60a1deed7092a60133f3314f
brunoness <bruno.h.c@hotmail.com>
2021-09-08 19:25:33 -0300

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
 import DonutChart from "components/DonutChart";
-import Footer from "components/Footer";
+import footer from "components/footer";
 import NavBar from "components/NavBar";
 
 function App() {
@@ -27,7 +27,7 @@ function App() {
         
       <DataTable />
     </div>
-    <Footer />
+    <footer />
     </>
   );
 }
