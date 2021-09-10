e81d0636bb76c6eaf56918ff62c02de6d856d798
brunoness <bruno.h.c@hotmail.com>
2021-09-09 21:08:50 -0300

    Layers

---
+++ backend/src/main/java/com/devofficer/dovendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devofficer.dovendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devofficer.dovendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
