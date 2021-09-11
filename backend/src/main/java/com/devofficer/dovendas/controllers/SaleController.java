96d8a2f425787cf60ad8b1a0b08ec5b0cada1e34
brunoness <bruno.h.c@hotmail.com>
2021-09-10 23:42:10 -0300

    Group by search

--- backend/src/main/java/com/devofficer/dovendas/controllers/SaleController.java
+++ backend/src/main/java/com/devofficer/dovendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.devofficer.dovendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devofficer.dovendas.dto.SaleDTO;
+import com.devofficer.dovendas.dto.SaleSuccessDTO;
+import com.devofficer.dovendas.dto.SaleSumDTO;
 import com.devofficer.dovendas.service.SaleService;
 
 @RestController
@@ -23,5 +27,17 @@ public class SaleController {
 		Page<SaleDTO> list = service.findAll(pageable);
 		return ResponseEntity.ok(list);
 		
+	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
+		return ResponseEntity.ok(list);
 	}
 }
--- backend/src/main/java/com/devofficer/dovendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devofficer/dovendas/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.devofficer.dovendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devofficer.dovendas.dto.SaleSuccessDTO;
+import com.devofficer.dovendas.dto.SaleSumDTO;
 import com.devofficer.dovendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+	@Query("SELECT new com.devofficer.dovendas.dto.SaleSumDTO(obj.seller, SUM (obj.amount)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.devofficer.dovendas.dto.SaleSuccessDTO(obj.seller, SUM (obj.visited), SUM (obj.deals)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
