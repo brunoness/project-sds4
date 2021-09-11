3c4a5794706b95d6f6f29e7b293b3fade78fb690
brunoness <bruno.h.c@hotmail.com>
2021-09-10 23:03:05 -0300

    Pagination

---
+++ backend/src/main/java/com/devofficer/dovendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devofficer.dovendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devofficer.dovendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
